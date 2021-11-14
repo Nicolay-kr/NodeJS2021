2ac1bab334e56c192dffd0acc16cf4992392b910
Nicolay-kr <nicolay.krischenovich@gmail.com>
2021-11-14 23:36:46 +0300

    feat: error message

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const { streams } = require("./streams.js");
 const { stdout, exit } = process;
 
-const allowedFlags = ['-c', '--config', '-i', '--input', '-o', '--output',];
+const allowedFlags = ['-c', '--config', '-i', '--input', '-o', '--output'];
 const flags = process.argv.slice(2);
+const flagsArr = Array.from(flags);
 
-// Array.from(flags).forEach(flag => {
-//     if(flag.startsWith("-")){
-//         if (!allowedFlags.includes(flag)) {
-//             stdout.write(`Flag ${flag} is incorrect`);
-//             stdout.write(`\nYou can use only -c, --config, -i, --input', -o, --output`);
-//             exit(9);
-//         }
-//     }
-// });
+flagsArr.forEach(flag => {
+    if(flag.startsWith("-")){
+        if (!allowedFlags.includes(flag)) {
+            stdout.write(`Flag ${flag} is incorrect`);
+            stdout.write(`\nYou can use only -c, --config, -i, --input', -o, --output`);
+            exit(9);
+        }
+        let numberOfRepetitions = flagsArr.filter(item=>item === flag.toString()).length
+        if (numberOfRepetitions > 1){
+            console.error(`Flag ${flag} repeated ${numberOfRepetitions}. Flags must not be repeated`);
+            exit(9);
+        }
+    }
+});
 // C1-C1-R0-A
 
 function getValue(shortFlaf,longFlaf) {

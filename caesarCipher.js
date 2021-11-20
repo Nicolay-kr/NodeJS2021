6a02cc7dc6c8022f528d48bed88006357f563258
Nicolay-kr <nicolay.krischenovich@gmail.com>
2021-11-20 15:45:06 +0300

    fix: cipher

--- caesarCipher.js
+++ caesarCipher.js
@@ -10,13 +10,13 @@ exports.caesarCipher = (text,encode=true) => {
         
         if(alphabet.includes(char)){
             let pos = (alphabet.indexOf(char) + shift);
-            pos = pos < 0? alphabet.length + pos : pos;
+            pos = pos < 0? alphabet.length + pos : pos > alphabet.length-1? pos-(alphabet.length):pos;
             return alphabet[pos];
         }
 
         if(alphabetUpper.includes(char)){
             let pos = (alphabetUpper.indexOf(char) + shift);
-            pos = pos < 0? alphabetUpper.length + pos : pos;
+            pos = pos < 0? alphabetUpper.length + pos : pos > alphabetUpper.length-1? pos-(alphabetUpper.length):pos;
             return alphabetUpper[pos];
         }
         
